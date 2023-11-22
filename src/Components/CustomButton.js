84be09909c24953de4e6eece5a32e1ee08ad159b
NazrinOsmanova <nazrinosmanovaa@gmail.com>
2023-11-22 17:54:27 +0400

    add2

--- src/Components/CustomButton.js
+++ src/Components/CustomButton.js
@@ -1,11 +1,19 @@
 import React from 'react'
 
-const CustomButton = () => {
-    const warnMessage = () => {
+const CustomButton = (e) => {
+    const warnMessage = (e) => {
         alert("Warn Message")
+        e.target.classList.add("red");
     }
-    const hint = () => {
-        alert("Hint")
+    const hint = (e) => {
+        alert("Hint");
+        e.target.classList.add("italic");
+    }
+    const hint2 = (e) => {
+        e.target.classList.remove("italic");
+    }
+    const warnMessage2 = (e) => {
+        e.target.classList.remove("red");
     }
     const text = (e) => {
         e.target.previousElementSibling.classList.toggle("new")
@@ -14,8 +22,8 @@ const CustomButton = () => {
         <div className='customButton'>
             <p>Hello World</p>
             <button onClick={text}>Text</button>
-            <button onClick={warnMessage}>Warn Message</button>
-            <button onMouseOver={hint}>Hint</button>
+            <button onClick={warnMessage} onMouseOut={warnMessage2}>Warn Message</button>
+            <button onMouseOver={hint} onMouseOut={hint2}>Hint</button>
         </div>
     )
 }
